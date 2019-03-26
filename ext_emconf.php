44d513a712113e3a7f24d71009a8c05c5be55ad4
Jan Haffner <jan.haffner@dkd.de>
2019-03-26 17:23:26 +0100

    [TASK] Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,8 +26,8 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-7.6.99',
-      'php' => '5.5.0-7.1.99',
+      'typo3' => '7.6.0-8.7.99',
+      'php' => '5.5.0-7.2.99',
       'caretaker' => '0.3.0-0.0.0',
     ),
     'conflicts' => 
